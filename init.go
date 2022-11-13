8d077f744d4b17d607f99456957c442f5aa99f53
Aamir Maniar <aamironline@gmail.com>
2022-11-13 14:45:15 +0530

    Example correction in docs!

--- init.go
+++ init.go
@@ -3,21 +3,21 @@ package signals
 // New createa a new signal that can be used to emit and listen to events
 // synchronously.
 // example:
-// package main
+//  package main
 //
-// import (
-// 	"fmt"
+//  import (
+// 	  "fmt"
 //
-// 	"github.com/maniartech/signals"
-// )
+// 	  "github.com/maniartech/signals"
+//  )
 //
-// func main() {
-// 	s := signals.New[int]()
-// 	s.Add(func(v int) {
-// 		fmt.Println(v)
-// 	})
-// 	s.Emit(1)
-// }
+//  func main() {
+//  	s := signals.New[int]()
+// 	  s.Add(func(v int) {
+// 		  fmt.Println(v)
+// 	  })
+// 	  s.Emit(1)
+//  }
 //
 func New[T any]() Signal[T] {
 
@@ -32,21 +32,21 @@ func New[T any]() Signal[T] {
 // signals but don't want to block the current goroutine. It does not guarantee
 // the type safety of the emitted value.
 // example:
+//
 // package main
 //
-// import (
-// 	"fmt"
+//  import (
+// 	  "fmt"
+//    "github.com/maniartech/signals"
+//  )
 //
-// 	"github.com/maniartech/signals"
-// )
-//
-// func main() {
-// 	s := signals.NewAsync[int]()
-// 	s.Add(func(v int) {
-// 		fmt.Println(v)
-// 	})
-// 	s.Emit(1)
-// }
+//  func main() {
+//    s := signals.NewAsync[int]()
+// 	  s.Add(func(v int) {
+// 		  fmt.Println(v)
+// 	  })
+// 	  s.Emit(1)
+//  }
 //
 func NewAsync[T any]() Signal[T] {
 	s := signal[T]{
