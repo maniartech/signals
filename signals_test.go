8231ca4d9dec88355bca21fe58105ff4629a8a4b
Aamir Maniar <aamironline@gmail.com>
2022-11-14 18:14:03 +0530

    Test updates

--- signals_test.go
+++ signals_test.go
@@ -1,40 +1,15 @@
 package signals_test
 
 import (
+	"reflect"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/maniartech/signals"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSignal(t *testing.T) {
-
-	var count int
-	wg := &sync.WaitGroup{}
-	wg.Add(6)
-
-	testSignal := signals.New[int]()
-	testSignal.Add(func(v int) {
-		count += 1
-		wg.Done()
-	})
-	testSignal.Add(func(v int) {
-		count += 1
-		wg.Done()
-	})
-
-	testSignal.Emit(1)
-	testSignal.Emit(2)
-	testSignal.Emit(3)
-
-	assert.Less(t, 0, testSignal.Len()*3)
-	wg.Wait()
-
-	assert.Equal(t, 6, count)
-}
-
-func TestSignalSync(t *testing.T) {
 	testSignal := signals.New[int]()
 
 	results := make([]int, 0)
@@ -50,5 +25,44 @@ func TestSignalSync(t *testing.T) {
 	testSignal.Emit(2)
 	testSignal.Emit(3)
 
-	assert.Equal(t, []int{1, 1, 2, 2, 3, 3}, results)
+	if len(results) != 6 {
+		t.Error("Count must be 6")
+	}
+
+	if reflect.DeepEqual(results, []int{1, 1, 2, 2, 3, 3}) == false {
+		t.Error("Results must be [1, 1, 2, 2, 3, 3]")
+	}
+}
+
+func TestSignalAsync(t *testing.T) {
+
+	var count int
+	wg := &sync.WaitGroup{}
+	wg.Add(6)
+
+	testSignal := signals.NewAsync[int]()
+	testSignal.Add(func(v int) {
+		time.Sleep(100 * time.Millisecond)
+		count += 1
+		wg.Done()
+	})
+	testSignal.Add(func(v int) {
+		time.Sleep(100 * time.Millisecond)
+		count += 1
+		wg.Done()
+	})
+
+	testSignal.Emit(1)
+	testSignal.Emit(2)
+	testSignal.Emit(3)
+
+	if count >= 6 {
+		t.Error("Not asynchronus! count must be less than 6")
+	}
+
+	wg.Wait()
+
+	if count != 6 {
+		t.Error("Count must be 6")
+	}
 }
