07081d20a3e96fe147b329f960c61bd724c5d1f5
Aamir Maniar <aamironline@gmail.com>
2023-12-26 22:36:18 +0530

    Implement AsyncSignal and SyncSignal structs for concurrent and synchronous signal handling

--- signals_async.go
+++ signals_async.go
@@ -2,16 +2,22 @@ package signals
 
 import "context"
 
-// AsyncSignal is the implementation of the Signal interface.
+// AsyncSignal is a struct that implements the Signal interface.
 // It provides the same functionality as the SyncSignal but
-// the listeners are called in a separate goroutine.
+// the listeners are called in a separate goroutine. This means that
+// the listeners can run concurrently, not blocking the main thread.
+// The type parameter `T` is a placeholder for any type.
 type AsyncSignal[T any] struct {
 	BaseSignal[T]
 }
 
 // Emit notifies all subscribers of the signal and passes the payload.
+// The payload is of the same type as the AsyncSignal's type parameter `T`.
+// The method iterates over the subscribers slice of the AsyncSignal,
+// and for each subscriber, it calls the subscriber's listener function in a new goroutine,
+// passing the context and the payload.
 // If the context has a deadline or cancellable property, the listeners
-// must respect it.
+// must respect it. This means that the listeners should stop processing when the context is cancelled.
 func (s *AsyncSignal[T]) Emit(ctx context.Context, payload T) {
 	for _, sub := range s.subscribers {
 		go sub.listener(ctx, payload)
--- signals_sync.go
+++ signals_sync.go
@@ -2,16 +2,23 @@ package signals
 
 import "context"
 
-// SyncSignal is the implementation of the Signal interface.
-//
+// SyncSignal is a struct that implements the Signal interface.
+// It provides a synchronous way of notifying all subscribers of a signal.
+// The type parameter `T` is a placeholder for any type.
 type SyncSignal[T any] struct {
 	BaseSignal[T]
 }
 
-// Emit notifies all subscribers of the signal and passes the payload.
+// Emit is a method on the SyncSignal struct.
+// It notifies all subscribers of the signal and passes the payload.
+// The payload is of the same type as the SyncSignal's type parameter `T`.
+// The method iterates over the subscribers slice of the SyncSignal,
+// and for each subscriber, it calls the subscriber's listener function,
+// passing the context and the payload.
 // If the context has a deadline or cancellable property, the listeners
-// must respect it. If the signal is async, the listeners are called
-// in a separate goroutine.
+// must respect it. This means that the listeners should stop processing when the context is cancelled.
+// Unlike the AsyncSignal's Emit method, this method does not call the listeners in separate goroutines,
+// so the listeners are called synchronously, one after the other.
 func (s *SyncSignal[T]) Emit(ctx context.Context, payload T) {
 	for _, sub := range s.subscribers {
 		sub.listener(ctx, payload)
