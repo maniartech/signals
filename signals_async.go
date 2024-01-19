666e991d4354543124ce41efaeadc0622566b2f4
Aamir Maniar <aamironline@gmail.com>
2024-01-19 16:11:23 +0530

    Refactor signal implementation to support asynchronous and synchronous emitting

--- signals_async.go
+++ signals_async.go
@@ -1,25 +1,56 @@
 package signals
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 // AsyncSignal is a struct that implements the Signal interface.
-// It provides the same functionality as the SyncSignal but
-// the listeners are called in a separate goroutine. This means that
-// the listeners can run concurrently, not blocking the main thread.
-// The type parameter `T` is a placeholder for any type.
+// This is the default implementation. It provides the same functionality as
+// the SyncSignal but the listeners are called in a separate goroutine.
+// This means that all listeners are called asynchronously. However, the method
+// waits for all the listeners to finish before returning. If you don't want
+// to wait for the listeners to finish, you can call the Emit method
+// in a separate goroutine.
 type AsyncSignal[T any] struct {
 	BaseSignal[T]
+
+	mu sync.Mutex
 }
 
-// Emit notifies all subscribers of the signal and passes the payload.
-// The payload is of the same type as the AsyncSignal's type parameter `T`.
-// The method iterates over the subscribers slice of the AsyncSignal,
-// and for each subscriber, it calls the subscriber's listener function in a new goroutine,
-// passing the context and the payload.
+// Emit notifies all subscribers of the signal and passes the payload in a
+// asynchronous way.
+//
 // If the context has a deadline or cancellable property, the listeners
-// must respect it. This means that the listeners should stop processing when the context is cancelled.
+// must respect it. This means that the listeners should stop processing when
+// the context is cancelled. While emtting it calls the listeners in separate
+// goroutines, so the listeners are called asynchronously. However, it
+// waits for all the listeners to finish before returning. If you don't want
+// to wait for the listeners to finish, you can call the Emit method. Also,
+// you must know that Emit does not guarantee the type safety of the emitted value.
+//
+// Example:
+//
+//	signal := signals.New[string]()
+//	signal.AddListener(func(ctx context.Context, payload string) {
+//		// Listener implementation
+//		// ...
+//	})
+//
+//	signal.Emit(context.Background(), "Hello, world!")
 func (s *AsyncSignal[T]) Emit(ctx context.Context, payload T) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	var wg sync.WaitGroup
+
 	for _, sub := range s.subscribers {
-		go sub.listener(ctx, payload)
+		wg.Add(1)
+		go func(listener func(context.Context, T)) {
+			defer wg.Done()
+			listener(ctx, payload)
+		}(sub.listener)
 	}
+
+	wg.Wait()
 }
--- signals_sync.go
+++ signals_sync.go
@@ -9,16 +9,27 @@ type SyncSignal[T any] struct {
 	BaseSignal[T]
 }
 
-// Emit is a method on the SyncSignal struct.
-// It notifies all subscribers of the signal and passes the payload.
+// Emit notifies all subscribers of the signal and passes the payload in a
+// synchronous way.
+//
 // The payload is of the same type as the SyncSignal's type parameter `T`.
 // The method iterates over the subscribers slice of the SyncSignal,
 // and for each subscriber, it calls the subscriber's listener function,
 // passing the context and the payload.
 // If the context has a deadline or cancellable property, the listeners
-// must respect it. This means that the listeners should stop processing when the context is cancelled.
-// Unlike the AsyncSignal's Emit method, this method does not call the listeners in separate goroutines,
-// so the listeners are called synchronously, one after the other.
+// must respect it. This means that the listeners should stop processing when
+// the context is cancelled. Unlike the AsyncSignal's Emit method, this method
+// does not call the listeners in separate goroutines, so the listeners are
+// called synchronously, one after the other.
+//
+// Example:
+//	signal := signals.NewSync[string]()
+//	signal.AddListener(func(ctx context.Context, payload string) {
+//		// Listener implementation
+//		// ...
+//	})
+//
+//	signal.Emit(context.Background(), "Hello, world!")
 func (s *SyncSignal[T]) Emit(ctx context.Context, payload T) {
 	for _, sub := range s.subscribers {
 		sub.listener(ctx, payload)
