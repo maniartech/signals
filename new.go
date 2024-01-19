4bcadf1ac80d013b7ea585ef00aed8a9d5d1d757
Aamir Maniar <aamironline@gmail.com>
2024-01-19 16:11:15 +0530

    Refactor signal creation and usage

--- new.go
+++ new.go
@@ -1,24 +1,15 @@
 package signals
 
-// NewSync createa a new signal that can be used to emit and listen to events
+// NewSync creates a new signal that can be used to emit and listen to events
 // synchronously.
-// example:
-//  package main
 //
-//  import (
-// 	  "fmt"
-//
-// 	  "github.com/maniartech/signals"
-//  )
-//
-//  func main() {
-//  	s := signals.NewSync[int]()
-// 	  s.AddListener(func(ctx context.Context, v int) {
-// 		  fmt.Println(v)
-// 	  })
-// 	  s.Emit(1)
-//  }
-//
+// Example:
+//  signal := signals.NewSync[int]()
+//  signal.AddListener(func(ctx context.Context, payload int) {
+//      // Listener implementation
+//      // ...
+//  })
+//  signal.Emit(context.Background(), 42)
 func NewSync[T any]() Signal[T] {
 	s := &SyncSignal[T]{}
 	s.Reset()
@@ -26,27 +17,15 @@ func NewSync[T any]() Signal[T] {
 }
 
 // New creates a new signal that can be used to emit and listen to events
-// asynchronously. When emitting an event, the signal will call all the
-// listeners in a separate goroutine. This is useful when you want to emit
-// signals but don't want to block the current goroutine. It does not guarantee
-// the type safety of the emitted value.
-// example:
+// asynchronously.
 //
-//  package main
-//
-//  import (
-// 	  "fmt"
-//    "github.com/maniartech/signals"
-//  )
-//
-//  func main() {
-//    s := signals.New[int]()
-// 	  s.AddListener(func(ctx context.Context, v int) {
-// 		  fmt.Println(v)
-// 	  })
-// 	  s.Emit(1)
-//  }
-//
+// Example:
+//  signal := signals.New[int]()
+//  signal.AddListener(func(ctx context.Context, payload int) {
+//      // Listener implementation
+//      // ...
+//  })
+//  signal.Emit(context.Background(), 42)
 func New[T any]() Signal[T] {
 	s := &AsyncSignal[T]{}
 	s.Reset() // Reset the signal
